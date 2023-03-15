ce4703cae12a92c36500f5941581f69f17696d1b
Harry Wijnschenk <harryw@Harrys-Mac-mini.local>
2023-03-15 17:41:23 +0000

    API Auth fixes

--- laravel/database/migrations/2014_10_12_000000_create_users_table.php
+++ laravel/database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,7 +16,6 @@ return new class extends Migration
             $table->string('first_name');
             $table->string('last_name');
             $table->string('email')->unique();
-            $table->date('data_of_birth');
             $table->boolean('is_admin');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
