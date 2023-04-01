7806d11a5530b4db0400b373843cbd9357ba4240
Harry41348 <hvalentinow@gmail.com>
2023-04-01 12:30:20 +0100

    Sidebar responsive design

--- react/src/components/Sidebar.jsx
+++ react/src/components/Sidebar.jsx
@@ -35,45 +35,94 @@ function Sidebar() {
   };
 
   return (
-    <aside className={classes.sidebar}>
-      <div>
-        <h1 className={classes.heading}>Mixxy</h1>
-        <Link className="btn-sidebar" to="/">
-          <AiFillHome />
-          <span>Dashboard</span>
-        </Link>
-        <Link className="btn-sidebar" to="/recipes">
-          <ImBook />
-          <span>Recipes</span>
-        </Link>
-        <Link className="btn-sidebar" to="/my-bar">
-          <FaGlassMartiniAlt />
-          My bar
-        </Link>
-        <a className="btn-sidebar" href="#">
-          <FaGraduationCap />
-          Learn
-        </a>
+    <>
+      <div className={classes.desktop} role="navigation">
+        <aside className={classes.sidebar}>
+          <div>
+            <h1 className={classes.heading}>Mixxy</h1>
+            <Link className="btn-sidebar" to="/">
+              <AiFillHome />
+              Dashboard
+            </Link>
+            <Link className="btn-sidebar" to="/recipes">
+              <ImBook />
+              Recipes
+            </Link>
+            <Link className="btn-sidebar" to="/my-bar">
+              <FaGlassMartiniAlt />
+              My bar
+            </Link>
+            <a className="btn-sidebar" href="#">
+              <FaGraduationCap />
+              Learn
+            </a>
+          </div>
+          {!token && (
+            <div>
+              <Link className="btn-sidebar" to="/login">
+                Login
+              </Link>
+              <Link className="btn-sidebar" to="/register">
+                Register
+              </Link>
+            </div>
+          )}
+          {token && (
+            <div>
+              <a className="btn-sidebar" href="#" onClick={onLogout}>
+                Logout
+              </a>
+              <p className={classes.profile}>{user.first_name}</p>
+            </div>
+          )}
+        </aside>
       </div>
-      {!token && (
-        <div>
-          <Link className="btn-sidebar" to="/login">
-            Login
-          </Link>
-          <Link className="btn-sidebar" to="/register">
-            Register
-          </Link>
+
+      <div className={classes.mobile} role="navigation">
+        <div className={classes.menuToggle} id="menuToggle">
+          <input type="checkbox" id="navMenu" />
+          <span></span>
+          <span></span>
+          <span></span>
+          <aside className={classes.sidebarMobile}>
+            <div>
+              <h1 className={classes.heading}>Mixxy</h1>
+              <Link className="btn-sidebar" to="/">
+                Dashboard
+              </Link>
+              <Link className="btn-sidebar" to="/recipes">
+                Recipes
+              </Link>
+              <Link className="btn-sidebar" to="/my-bar">
+                My bar
+              </Link>
+              <a className="btn-sidebar" href="#">
+                Learn
+              </a>
+            </div>
+            {!token && (
+              <div>
+                <Link className="btn-sidebar" to="/login">
+                  Login
+                </Link>
+                <Link className="btn-sidebar" to="/register">
+                  Register
+                </Link>
+              </div>
+            )}
+            {token && (
+              <div>
+                <a className="btn-sidebar" href="#" onClick={onLogout}>
+                  Logout
+                </a>
+                <p className={classes.profile}>{user.first_name}</p>
+              </div>
+            )}
+          </aside>
         </div>
-      )}
-      {token && (
-        <div>
-          <a className="btn-sidebar" href="#" onClick={onLogout}>
-            Logout
-          </a>
-          <p className={classes.profile}>{user.first_name}</p>
-        </div>
-      )}
-    </aside>
+        {/* </div> */}
+      </div>
+    </>
   );
 }
 
