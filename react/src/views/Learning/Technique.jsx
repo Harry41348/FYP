39d185a37009122702e92eb6f20e04decf4b3e76
Harry41348 <51890894+Harry41348@users.noreply.github.com>
2023-04-13 19:14:19 +0100

    Learning content updated (#27)

--- react/src/views/Learning/Technique.jsx
+++ react/src/views/Learning/Technique.jsx
@@ -8,28 +8,69 @@ function Technique() {
   const accordions = [
     {
       accordion: "Stirring",
-      typography:
-        "This method is most used when cocktails are made of spirits and liqueurs. The Technique are put into a glass/mixing glass with some ice and stirred using a bar spoons. This helps prevent the ice diluting the drink.",
+      typography: (
+        <>
+          Stirring is used when cocktails are made of spirits and liqueurs. The
+          ingredients are poured into a glass or mixing glass with some ice and
+          stirred using a bar spoons. This helps prevent the ice diluting the
+          drink, whilst keeping it chilled and combining the alcohols together.
+        </>
+      ),
     },
     {
       accordion: "Shaking",
-      typography:
-        "When you think of cocktail making, you likely think of shaking. This is when ingredients are placed into a shaker, and thoroughly shaken until the ingredients have mixed, and are cold from the ice. This method is most used for drinks with fruit juices, egg, or cream.",
+      typography: (
+        <>
+          A very common technique in making cocktails. The ingredients are
+          placed into a shaker (usually filled with ice) and thoroughly shaken
+          until the ingredients have mixed and chilled. This method is most
+          often used for drinks with fruit juices, egg, or cream and then
+          strained for a smooth drink, that is sometimes frothy depending on the
+          ingredients.
+        </>
+      ),
     },
     {
       accordion: "Blending",
-      typography:
-        "Blending is useful when a drink has solid foods in it, such as strawberries, which when blended provide a smooth drink. Adding ice to the blending can also give a slushy texture. This is also an easy way to make multiple drinks in one.",
+      typography: (
+        <>
+          Blending is useful when a drink has solid foods in it, such as fruits,
+          which when blended provide a smooth drink. Adding ice to the blending
+          can also give a slushy texture. This is also an easy way to make
+          multiple drinks in one. One of the more popular drinks for blending is
+          the Strawberry Daquiri.
+        </>
+      ),
     },
     {
       accordion: "Layering",
-      typography:
-        "This technique gives drinks an elegant look, with different colours being layered on top of one another. To stop the liquids from merging, this method requires drinks to be poured over the back of a spoon into a glass. There is a bit of theory that goes into this technique.",
+      typography: (
+        <>
+          This technique gives drinks an elegant look, with different colours
+          being layered on top of one another. To stop the liquids from merging,
+          this method requires drinks to be poured over the back of a spoon into
+          a glass. Another way to do this is to pour drinks over the twisted
+          part of a bar spoon.
+          <br />
+          Theory: The drinks are poured in order of their specific gravity. The
+          heaviest drinks at the bottom, the lightest on top. It is hard to know
+          the exact density of each ingredient, however the more sugar an
+          ingredient has, the heavier it will be. Syrups are generally the
+          heaviest ingredient. Alcohol is generally light, making liqueurs
+          lighter than syrups, and spirits as the lightest ingredient. The
+          higher the proof, the lighter it is.
+        </>
+      ),
     },
     {
       accordion: "Building",
-      typography:
-        "This is a basic technique that can be done straight into the glass, by putting in the ingredients one at a time over ice and giving them a simple stir.",
+      typography: (
+        <>
+          This is a basic technique that is done straight into the glass, by
+          pouring the ingredients one at a time over ice and giving them a
+          simple stir.
+        </>
+      ),
     },
   ];
 
