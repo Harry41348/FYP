3998ec29a5b457663c119eae33b6c8c5487bf3de
Harry41348 <51890894+Harry41348@users.noreply.github.com>
2023-04-13 19:15:31 +0100

    Content Updated (#28)
    - Feedback implemented
    - Learning content updated

--- react/src/views/Learning/Equipment.jsx
+++ react/src/views/Learning/Equipment.jsx
@@ -7,39 +7,104 @@ import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 function Equipment() {
   const accordions = [
     {
-      accordion: "Shakers",
-      typography:
-        "These are a core tool for creating cocktails. Shakers are used to mix ingredients together thoroughly, usually when the drink has mixers in.",
+      accordion: "Cocktail Shakers",
+      typography: (
+        <>
+          Shakers are a tool used for mixing ingredients together thoroughly,
+          whilst chilling them at the same time. Drinks are chilled faster with
+          this method than stirring, due to the drinks repeatedly being exposed
+          to the ice at a faster rate. With this tool, you want to use ice cubes
+          or chunks, rather than smaller fragments of ice. This prevents
+          cocktails being over-diluted with water. The chunks of ice will still
+          dilute, giving the cocktail a smoother taste by mellowing out the
+          harsher tastes.
+          <br /> There are a few types of shakers, the most popular in
+          bartending is the Boston shaker, which has a larger volume allowing
+          for multiple cocktails to be made at once. Another is the cobbler,
+          which is a 3-piece shaker, with a built-in strainer. And lastly is the
+          French/Parisian shaker which is a similar shape to the cobbler without
+          the built in strainer.
+        </>
+      ),
     },
     {
       accordion: "Strainers",
-      typography:
-        "These are useful for making cocktails smooth, by catching pulp and ice.",
+      typography: (
+        <>
+          There are a few types of strainers used for making cocktails. The most
+          common are the Hawthorne and the fine mesh strainer. These are
+          excellent for filtering out pulp and ice, making for a smoother drink.
+          Other than the built in strainer in the cobbler shaker, there is also
+          the julep strainer, a bowl-shaped strainer which used to be used for
+          drinking mint juleps, but also works for filtering out pulp and ice.
+        </>
+      ),
     },
     {
       accordion: "Jiggers",
-      typography:
-        "A tool used for measuring liquid ounces quickly. The most common one is to have a double-sided jigger for measure 1 and 2 ounces, ideal for most cocktails.",
+      typography: (
+        <>
+          A jigger is 1.5 ounces. This tool was named after this measurement and
+          consists of two sides measuring typically 1 ounce and 1.5 ounces, but
+          can range from half an ounce to 2.5 ounces. They allow bartenders to
+          rapidly pour accurate measures of ingredients.
+        </>
+      ),
     },
     {
       accordion: "Bar Spoons",
-      typography:
-        "These are different from regular spoons, with a longer range to mix drinks from the bottom to the top, along with a twisted middle part to allow the user to mix with ease once they get the hang of it.",
+      typography: (
+        <p>
+          Bar spoons are longer, twisted spoons used for stirring drinks. There
+          are a few variations, but they all perform the same function. It makes
+          stirring easy once you get the technique down and helps reach the
+          bottom of the glass to ensure the drink is mixed all the way through.
+          To stir with it, you hold it between your thumb, middle and ring
+          finger like a pencil, and stir it along the outside of the glass.
+          <br /> Fun fact: The bar spoon can be used to layer drinks by pouring
+          onto the twisted part of the spoon and watch as the liquid swirls
+          down. This is a great way to impress guests rather than pouring onto
+          the back of a spoon.
+        </p>
+      ),
     },
     {
       accordion: "Bar Glasses",
-      typography:
-        "Bar glasses are essential for containing the drink made. Starting out, any regular glass can be used, but specific drinks are intended for certain glasses and can have a big impact of aesthetics. Examples include martini glasses for martinis, highball for long island iced teas, and rock glasses for an old fashioned. ",
+      typography: (
+        <>
+          Of course, a vital piece of equipment to have is the glass to drink
+          out of. There is a wide variety of glasses, and most drinks have a
+          specific glass it is intended for. Drinks will taste the same no
+          matter the glass, but you want drinks to be aesthetically pleasing to
+          impress your drinkers. The highball, rocks and coupe/martini glasses
+          are some great ones to start with. There are many glasses out there,
+          but a few other popular ones include: The collins glass, copper mug,
+          margarita glass, hurricane glass and the wine glass.
+        </>
+      ),
     },
     {
       accordion: "Garnishing Tools",
-      typography:
-        "Garnishes help to make cocktails more aesthetically pleasing. There are many tools intended for garnishing such as a paring knife, zester, peeler, toothpicks, clothespins, and more. ",
+      typography: (
+        <>
+          Garnishes help to make cocktails more aesthetically pleasing. There
+          are many tools intended for garnishing such as a paring knife, zester,
+          peeler, toothpicks, clothespins, and more. At home, garnishing can
+          seem pointless and a waste, but it really does elevate drinks to
+          another level and will impress guests/customers.
+        </>
+      ),
     },
     {
       accordion: "Additional Tools",
-      typography:
-        "There are many tools that are not used every day by mixologists but can be useful in specific use cases. Some include blenders, ice makers, funnels, absinthe spoon, citrus squeezer, and many more.",
+      typography: (
+        <>
+          There are many tools that are not used every day by mixologists but
+          are useful in specific use cases. Some include blenders, ice makers,
+          funnels, absinthe spoon, citrus squeezer, and there are many more
+          which can be discovered through finding recipes.
+        </>
+      ),
     },
   ];
 
--- react/src/views/Learning/Ingredients.jsx
+++ react/src/views/Learning/Ingredients.jsx
@@ -3,28 +3,66 @@ import AccordionSummary from "@mui/material/AccordionSummary";
 import AccordionDetails from "@mui/material/AccordionDetails";
 import Typography from "@mui/material/Typography";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import { Link } from "react-router-dom";
 
 function Ingredients() {
   const accordions = [
     {
       accordion: "Alcohol",
-      typography:
-        "This is the base for all cocktails, whilst a good tasting spirit/liqueur is important, it is a good idea to not get anything too expensive as most cocktails have many flavours and will waste the taste of the spirit. The base alcoholic ingredients to stock for a home bar are: vodka, gin, tequila, rum, whisk(e)y, and brandy. Over time, other spirits and liqueurs can be bought to create a variety of cocktails.",
+      typography: (
+        <>
+          Alcohol is the base for all cocktails (without alcohol it would be a
+          mocktail) and is usually either a spirit or a liqueur. The difference
+          is that a spirit is made from a grain or a fruit or vegetable sugar
+          which is fermented and distilled, whereas a liqueur is generally
+          sweeter and flavoured and is made from a liquor.
+          <br />A good tasting spirit/liqueur is important, but it is a good
+          idea to not get anything too expensive as most cocktails have many
+          flavours and this would cover up the complexities of the alcohol. Some
+          great ingredients to start off with include: vodka, gin, tequila,
+          light rum, dark rum and whisk(e)y. Over time, other spirits and
+          liqueurs can be bought to create a variety of cocktails. A more
+          extensive list can be found in the ingredient section of{" "}
+          <Link to="/my-bar">your bar</Link>.
+        </>
+      ),
     },
     {
       accordion: "Mixers",
-      typography:
-        "Mixers done well give drinks their great flavours and mask the flavour of the alcohol in many drinks. The main mixers are juices, citrus and fruits, syrups, and other liquids. The essentials include: Tonic water, ginger beer, club soda, lemonade, cola, lemons and limes, and sugar syrup.",
+      typography: (
+        <>
+          Mixers give drinks their great and unique flavours whilst reducing the
+          strong flavour of the alcohol. There is an abundance of mixers out
+          there, with the main ones including different juices, citrus, fruits,
+          syrups, and other liquids. Some great mixers to have stocked up
+          include: Ginger beer/ale, soda water, lemonade, cola and sugar syrup.
+          Things like juices and fruits can be bought when needed, but having
+          these other ingredients gives a great base to build from for many
+          cocktails.
+        </>
+      ),
     },
     {
       accordion: "Garnishes",
-      typography:
-        "Though these are not essential, they can give cocktails a boost in aesthetics, and some can help with the flavour. Some good to have garnishes include: Limes, lemons, oranges, maraschino cherries, salt, sugar cubes, and green olives.",
+      typography: (
+        <>
+          Though these are not essential, they can give cocktails a boost in
+          aesthetics, and can help with the flavour in some cases. Some good to
+          have garnishes include: Limes, lemons, oranges, maraschino cherries,
+          salt, sugar cubes and green olives.
+        </>
+      ),
     },
     {
       accordion: "Ice",
-      typography:
-        "Ice is another essential ingredient, one that is easy to overlook. Without ice, a perfect cocktail can become undrinkable. Shop bought ice works well for taste, but ice moulds can make them much more aesthetic.",
+      typography: (
+        <>
+          Ice is essential and easily overlooked. You can do every step right in
+          making a cocktail, but without ice the drink will often become
+          undrinkable. Shop bought ice works well for taste and mixing, but ice
+          moulds can make them much more aesthetic and are cheaper to make.
+        </>
+      ),
     },
   ];
 
